240f1da2894f54d2a89f4e947d67913fc4b25a5b
Mathias Monnerville <monnerville@iap.fr>
2011-07-12 10:46:38 +0200

    Added SectionedAdapter support in AllLinesActivity

--- src/com/monnerville/tranports/herault/AllLinesActivity.java
+++ src/com/monnerville/tranports/herault/AllLinesActivity.java
@@ -3,6 +3,7 @@ package com.monnerville.tranports.herault;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -11,8 +12,14 @@ import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+import com.commonsware.android.listview.SectionedAdapter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
@@ -49,10 +56,8 @@ public class AllLinesActivity extends ListActivity
 
         try {
             List<BusLine> lines = manager.getBusLines();
-            ListAdapter adapter = new SimpleAdapter(this, getData(lines),
-                R.layout.all_lines_list_item, new String[] {"name", "directions"},
-                new int[] {android.R.id.text1, android.R.id.text2});
-            setListAdapter(adapter);
+            mAdapter.addSection(getString(R.string.all_lines_header), new LineListAdapter(this, R.layout.all_lines_list_item, lines));
+            setListAdapter(mAdapter);
         } catch(XmlPullParserException err) {
             Log.e("TAG", "Parsing error: " + err.getMessage());
         } catch(IOException err) {
@@ -60,42 +65,83 @@ public class AllLinesActivity extends ListActivity
         }
     }
 
-    private List getData(List<BusLine> lines) throws XmlPullParserException, IOException {
-        List<Map> data = new ArrayList<Map>();
-        for (BusLine line : lines) {
-            Map<String, String> m = new HashMap<String, String>();
-            m.put("name", line.getName());
-            String[] d = line.getDirections();
-            m.put("directions", d[0] + " - " + d[1]);
-            m.put("direction1", d[0]);
-            m.put("direction2", d[1]);
-            data.add(m);
+    private class LineListAdapter extends ArrayAdapter<BusLine> {
+        private int mResource;
+        private Context mContext;
+
+        LineListAdapter(Context context, int resource, List<BusLine> items) {
+            super(context, resource, items);
+            mResource = resource;
+            mContext = context;
         }
-        return data;
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout itemView;
+            BusLine line = getItem(position);
+
+            if (convertView == null) {
+                itemView = new LinearLayout(mContext);
+                LayoutInflater li = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                li.inflate(mResource, itemView, true);
+            }
+            else
+                itemView = (LinearLayout)convertView;
+
+            TextView name = (TextView)itemView.findViewById(android.R.id.text1);
+            name.setText(line.getName());
+            TextView direction = (TextView)itemView.findViewById(android.R.id.text2);
+            try {
+                String[] dirs = line.getDirections();
+                direction.setText(dirs[0] + " - " + dirs[1]);
+            } catch (XmlPullParserException ex) {
+                Logger.getLogger(AllLinesActivity.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(AllLinesActivity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return itemView;
+        }
     }
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        final Map<String, String> map = (Map)getListView().getItemAtPosition(position);
-        final String[] directions = new String[2];
-        directions[0] = map.get("direction1");
-        directions[1] = map.get("direction2");
-
-        if (directions[0] == null || directions[1] == null) {
-            Toast.makeText(this, R.string.toast_null_direction, Toast.LENGTH_SHORT).show();
-            return;
-        }
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(R.string.pick_direction_title);
-        builder.setItems(directions, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int item) {
-                Intent intent = new Intent(AllLinesActivity.this, BusLineActivity.class);
-                intent.putExtra("line", map.get("name"));
-                intent.putExtra("direction", directions[item]);
-                startActivity(intent);
+        final BusLine line = (BusLine)getListView().getItemAtPosition(position);
+        final String[] directions;
+        try {
+            directions = line.getDirections();
+            if (directions[0] == null || directions[1] == null) {
+                Toast.makeText(this, R.string.toast_null_direction, Toast.LENGTH_SHORT).show();
+                return;
             }
-        });
-        builder.show();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.pick_direction_title);
+            builder.setItems(directions, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int item) {
+                    Intent intent = new Intent(AllLinesActivity.this, BusLineActivity.class);
+                    intent.putExtra("line", line.getName());
+                    intent.putExtra("direction", directions[item]);
+                    startActivity(intent);
+                }
+            });
+            builder.show();
+        } catch (XmlPullParserException ex) {
+            Logger.getLogger(AllLinesActivity.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(AllLinesActivity.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+
+	final SectionedAdapter mAdapter = new SectionedAdapter() {
+        @Override
+		protected View getHeaderView(String caption, int index, View convertView, ViewGroup parent) {
+			TextView result = (TextView)convertView;
+			if (convertView == null) {
+				result = (TextView)getLayoutInflater().inflate(R.layout.list_header, null);
+			}
+			result.setText(caption);
+			return(result);
+		}
+	};
+
 }
