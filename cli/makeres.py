8bb552a00e71f88a7fcf3fa6e2a0a3cb1195545a
Mathias Monnerville <mathias@monnerville.com>
2011-07-21 22:01:17 +0200

    script fixes

--- cli/makeres.py
+++ cli/makeres.py
@@ -41,19 +41,19 @@ def makeXML(busline, directions, outfile):
     f.write("""<line id="%s">\n""" % busline)
     for data in directions:
         curDirection = data[-1]['city']
-        f.write(' ' * INDENT + """<direction id="%s" c="%s">\n""" % (curDirection.encode('latin-1'), dfltCirculationPolicy))
+        f.write(' ' * INDENT + """<direction id="%s" c="%s">\n""" % (curDirection.encode('utf-8'), dfltCirculationPolicy))
         curCity = None
         for station in data:
             city = station['city']
             if city != curCity:
                 if curCity != None:
                     f.write(' ' *2*INDENT + "</city>\n")
-                f.write(' ' *2*INDENT + """<city id="%s">\n""" % city.encode('latin-1'))
+                f.write(' ' *2*INDENT + """<city id="%s">\n""" % city.encode('utf-8'))
                 if city not in tmpCities:
                     tmpCities.append(city)
                     nbCities += 1
                 curCity = city
-            f.write(' ' *3*INDENT + """<station id="%s">\n""" % station['station'].encode('latin-1'))
+            f.write(' ' *3*INDENT + """<station id="%s">\n""" % station['station'].encode('utf-8'))
             for stop in station['stops']:
                 if type(stop) == types.TupleType:
                     f.write(' ' *4*INDENT + """<stop t="%s" c="%s"/>\n""" % (stop[0], stop[1]))
@@ -65,6 +65,8 @@ def makeXML(busline, directions, outfile):
             if station['station'] not in tmpStations:
                 tmpStations.append(station['station'])
                 nbStations += 1
+        f.flush()
+        f.write(' ' *2*INDENT + "</city>\n")
         f.write(' ' * INDENT + "</direction>\n")
         nbDirections += 1
     f.write("</line>")
@@ -117,7 +119,7 @@ def parse(infile):
         elif line.startswith(CITY_PAT):
             curCity = re.sub(CITY_PAT, '', line)
         elif line.startswith(BUSLINE_PAT):
-            busline = re.sub(BUSLINE_PAT, '', line).encode('latin-1')
+            busline = re.sub(BUSLINE_PAT, '', line).encode('utf-8')
         elif line.startswith(FROM_PAT):
             pass
         elif line.startswith(TO_PAT):
@@ -176,8 +178,11 @@ def main():
             makeXML(busline, directions, outfile)
 
         if options.globalxml:
+            dst = os.path.join(infile, 'lines.xml')
+            if os.path.exists(dst):
+                os.remove(dst)
             xmls = glob.glob(os.path.join(infile, '*.xml'))
-            f = open(os.path.join(infile, 'lines.xml'), 'w')
+            f = open(dst, 'w')
             f.write(XML_HEADER)
             f.write('<lines>\n')
             for src in xmls:
@@ -185,6 +190,7 @@ def main():
                 f.write(''.join(s.readlines()[2:]) + '\n')
                 f.flush()
                 s.close()
+            f.flush()
             f.write('</lines>\n')
             f.close()
             print "Generated global %s" % os.path.join(infile, 'lines.xml')
