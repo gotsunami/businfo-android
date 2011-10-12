5111ab800d03d99185d980d690466475a339c6bc
Mathias Monnerville <mathias@monnerville.com>
2011-10-12 23:29:50 +0200

    Adding support for SQL generation

--- cli/makeres.py
+++ cli/makeres.py
@@ -133,6 +133,189 @@ def makeXML(busline, directions, outfile):
     print "[%-15s] %-30s (Dir: %d, Cit: %2d, Stations: %2d, Stops: %2d)" % (busline, "Generated %s" % outfile, nbDirections, nbCities, nbStations, nbStops)
     if DEBUG: print directions
 
+def createDB():
+    # Create DB structure
+    print("""
+DROP TABLE IF EXISTS line;
+CREATE TABLE line (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT, 
+    from_city_id INTEGER, 
+    to_city_id INTEGER,
+    UNIQUE(name)
+);
+
+DROP TABLE IF EXISTS city;
+CREATE TABLE city (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT,
+    latitude REAL,
+    longitude REAL,
+    UNIQUE(name),
+    UNIQUE(latitude, longitude)
+);
+
+DROP TABLE IF EXISTS station;
+CREATE TABLE station (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    latitude REAL,
+    longitude REAL,
+    city_id INTEGER,
+    UNIQUE(name, city_id),
+    UNIQUE(latitude, longitude)
+);
+
+DROP TABLE IF EXISTS schedule;
+CREATE TABLE schedule (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    time DATETIME,
+    station_id INTEGER,
+);
+
+DROP TABLE IF EXISTS line_station;
+CREATE TABLE line_station (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    line_id INTEGER,
+    station_id INTEGER,
+    UNIQUE(line_id, station_id)
+);
+
+CREATE TRIGGER fki_line_from_city_id
+BEFORE INSERT ON line
+BEGIN
+    SELECT RAISE(ROLLBACK, 'insert on table "line" violates foreign key constraint "fk_from_city_id"')
+    WHERE (SELECT id FROM city WHERE id = NEW.from_city_id) IS NULL;
+END;
+
+CREATE TRIGGER fki_line_to_city_id
+BEFORE INSERT ON line
+BEGIN
+    SELECT RAISE(ROLLBACK, 'insert on table "line" violates foreign key constraint "fk_to_city_id"')
+    WHERE (SELECT id FROM city WHERE id = NEW.to_city_id) IS NULL;
+END;
+
+CREATE TRIGGER fki_station_city_id
+BEFORE INSERT ON station
+BEGIN
+    SELECT RAISE(ROLLBACK, 'insert on table "station" violates foreign key constraint "fk_city_id"')
+    WHERE (SELECT id FROM city WHERE id = NEW.city_id) IS NULL;
+END;
+
+CREATE TRIGGER fki_line_station_line_id
+BEFORE INSERT ON line_station
+BEGIN
+    SELECT RAISE(ROLLBACK, 'insert on table "line_station" violates foreign key constraint "fk_line_id"')
+    WHERE (SELECT id FROM line WHERE id = NEW.line_id) IS NULL;
+END;
+
+CREATE TRIGGER fki_line_station_station_id
+BEFORE INSERT ON line_station
+BEGIN
+    SELECT RAISE(ROLLBACK, 'insert on table "line_station" violates foreign key constraint "fk_station_id"')
+    WHERE (SELECT id FROM station WHERE id = NEW.station_id) IS NULL;
+END;
+
+CREATE TRIGGER fki_schedule_station_id
+BEFORE INSERT ON schedule
+BEGIN
+    SELECT RAISE(ROLLBACK, 'insert on table "schedule" violates foreign key constraint "fk_station_id"')
+    WHERE (SELECT id FROM station WHERE id = NEW.station_id) IS NULL;
+END;
+""");
+
+def makeSQL(sources):
+    global dfltCirculationPolicy
+    global g_cities
+
+    try:
+        f = open('/tmp/db.sql', 'w')
+    except IOError, e:
+        print "Error: %s" % e
+        sys.exit(1)
+
+    cities = set()
+    stations = set()
+    lines = set()
+    for src in sources:
+        busline, directions = parse(src)
+        #print directions[0]
+        lines.add(busline)
+        for direct in directions:
+            for data in direct:
+                cities.add(data['city'])
+                stations.add(data['station'])
+#    print cities, len(cities)
+#    print stations, len(stations)
+    k = 1
+    cs = []
+    for city in cities:
+        cs.append((k, city))
+        k += 1
+
+    print "Writing INSERTs for cities..."
+    for city in cs:
+        f.write("INSERT INTO city VALUES(%d, \"%s\", 0, 0);\n" % (city[0], city[1]))
+
+    print "Writing INSERTs for stations..."
+    for st in stations:
+        f.write("INSERT INTO station VALUES(\"%s\", 0, 0);\n" % st.encode('utf-8'))
+    print lines, len(lines)
+
+
+
+
+    f.close()
+    return
+
+    nbDirections = 0
+    nbCities = 0
+    nbStations = 0
+    nbStops = 0
+    # Used to count distinct entries
+    tmpCities = tmpStations = []
+
+#    f.write("CREATE TABLE )
+    f.write("""<line id="%s">\n""" % busline)
+    for data in directions:
+        curDirection = data[-1]['city']
+        f.write(' ' * INDENT + """<direction id="%s" c="%s">\n""" % (curDirection.encode('utf-8'), dfltCirculationPolicy))
+        curCity = None
+        for station in data:
+            city = station['city']
+            if city != curCity:
+                if curCity != None:
+                    f.write(' ' *2*INDENT + "</city>\n")
+                f.write(' ' *2*INDENT + """<city id="%s">\n""" % city.encode('utf-8'))
+                if city not in tmpCities:
+                    tmpCities.append(city)
+                    nbCities += 1
+                curCity = city
+                if city not in g_cities:
+                    g_cities.append(city)
+            f.write(' ' *3*INDENT + """<station id="%s">\n""" % station['station'].encode('utf-8'))
+            for stop in station['stops']:
+                if type(stop) == types.TupleType:
+                    f.write(' ' *4*INDENT + """<s t="%s" c="%s"/>\n""" % (stop[0], stop[1]))
+                else:
+                    f.write(' ' *4*INDENT + """<s t="%s"/>\n""" % stop)
+                
+                nbStops += 1
+            f.write(' ' *3*INDENT + "</station>\n")
+            if station['station'] not in tmpStations:
+                tmpStations.append(station['station'])
+                nbStations += 1
+        f.flush()
+        f.write(' ' *2*INDENT + "</city>\n")
+        f.write(' ' * INDENT + "</direction>\n")
+        nbDirections += 1
+    f.write("</line>")
+    f.close()
+
+    g_cities.sort()
+    print "[%-15s] %-30s (Dir: %d, Cit: %2d, Stations: %2d, Stops: %2d)" % (busline, "Generated %s" % outfile, nbDirections, nbCities, nbStations, nbStops)
+    if DEBUG: print directions
+
 def parse(infile):
     """
     Simple raw data parser
@@ -217,13 +400,18 @@ def parse(infile):
 def main():
     global DEBUG
 
-    parser = OptionParser(usage="Usage: %prog [-d|-g|--gps|--gps-cache file] (raw_line.txt|dir)")
+    parser = OptionParser(usage="""
+%prog [-d|-g|--gps|--gps-cache file] (raw_line.txt|dir)
+
+Default behaviour is to output XML content. Use --sql to instead
+generate SQL data.""")
     parser.add_option("-d", action="store_true", dest="debug", default=False, help='more debugging')
     parser.add_option("-v", '--verbose', action="store_true", dest="verbose", default=False, help='verbose output')
     parser.add_option("-g", action="store_true", dest="globalxml", default=False, help='generates global lines.xml')
     parser.add_option("", '--gps', action="store_true", dest="getgps", default=False, help='retreives cities GPS coordinates')
     parser.add_option("", '--gps-cache', action="store", dest="gpscache", default=GPS_CACHE_FILE, 
         help='use gps cache file')
+    parser.add_option("", '--sql', action="store_true", dest="sql", default=False, help='generates SQL content only')
     options, args = parser.parse_args()
 
     if len(args) != 1:
@@ -235,10 +423,17 @@ def main():
     if os.path.isdir(infile):
         sources = glob.glob(os.path.join(infile, '*.txt'))
         sources.sort()
+        if options.sql:
+            createDB()
+            makeSQL(sources)#busline, directions, outfile)
+            return
+
         for src in sources:
-            outfile = os.path.join(TMP_DIR, os.path.basename(src[:src.rfind('.')] + '.xml'))
             busline, directions = parse(src)
-            makeXML(busline, directions, outfile)
+            ext = '.xml'
+            f = makeXML
+            outfile = os.path.join(TMP_DIR, os.path.basename(src[:src.rfind('.')] + ext))
+            f(busline, directions, outfile)
 
         if options.globalxml:
             dst = os.path.join(TMP_DIR, 'lines.xml')
