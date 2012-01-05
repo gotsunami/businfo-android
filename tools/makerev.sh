126f6f9207db4d2753d6ddfd50b4816f491e5a69
Mathias Monnerville <mathias@monnerville.com>
2012-01-05 22:25:32 +0100

    makerev.sh now uses git to get HEAD commit ID

--- tools/makerev.sh
+++ tools/makerev.sh
@@ -4,5 +4,5 @@ OFILE=res/values/revision.xml
 
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" > $OFILE
 echo "<resources>" >> $OFILE
-echo "<string name=\"app_revision\">`hg tip --template '{rev}:{node|short}'`@`hostname -s`</string>" >> $OFILE
+echo "<string name=\"app_revision\">`git rev-parse HEAD| cut -b 1-12`@`hostname -s`</string>" >> $OFILE
 echo "</resources>" >> $OFILE
