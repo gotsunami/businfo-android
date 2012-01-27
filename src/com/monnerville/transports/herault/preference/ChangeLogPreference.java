256e2a63cd68fcec2ae53a70d63d3a0d4f9f4b67
Mathias Monnerville <mathias@monnerville.com>
2012-01-27 23:55:01 +0100

    Some work on the ChangeLog section in settings

---
+++ src/com/monnerville/transports/herault/preference/ChangeLogPreference.java
@@ -0,0 +1,12 @@
+package com.monnerville.transports.herault.preference;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class ChangeLogPreference extends DialogPreference
+{
+	public ChangeLogPreference(Context oContext, AttributeSet attrs) {
+		super(oContext, attrs);		
+	}
+}
