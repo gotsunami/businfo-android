56c28a009de698f014b2add46bc963f8fc9e0afa
Mathias Monnerville <mathias@monnerville.com>
2012-04-12 08:49:39 +0200

    Show branch name in revision tag if not master

--- tools/makerev.sh
+++ tools/makerev.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 OFILE=res/values/revision.xml
+BRANCH=-`git branch|grep ^\*|sed 's,^\* ,,'`
+test "$BRANCH" = "-master" && unset BRANCH
 
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" > $OFILE
 echo "<resources>" >> $OFILE
-echo "<string name=\"app_revision\">`git rev-parse HEAD| cut -b 1-7`@`hostname -s`</string>" >> $OFILE
+echo "<string name=\"app_revision\">`git rev-parse HEAD|cut -b 1-7`$BRANCH@`hostname -s`</string>" >> $OFILE
 echo "</resources>" >> $OFILE
