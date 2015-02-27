485a5a04ce400a1cebb759e344b81cb33a133ef1
matm <mathias@monnerville.com>
2015-02-27 12:51:44 +0100

    Delete old stuff

--- tools/gobuild-linux-386.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Called by Ant task -native-libs
-
-GOARCH=386 GOOS=linux go build $@
--- tools/gobuild-linux-arm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Called by Ant task -native-libs
-
-GOARCH=arm GOOS=linux go build $@
